c6ed3af911c10add43a54d302cbc6a0c10f253e3
Michael J Erwin <michaeljameserwin@gmail.com>
2023-09-23 18:05:01 -0400

    ArrayList: finish fake array list impl

--- src/array_list.rs
+++ src/array_list.rs
@@ -14,7 +14,7 @@ export default class ArrayList<T> {
     prepend(item: T): void {}
     insertAt(item: T, idx: number): void {}
     append(item: T): void {}
-    remove(item: T): T | undefined {}
+    TODO: remove(item: T): T | undefined {}
     get(idx: number): T | undefined {}
     removeAt(idx: number): T | undefined {}
 }
@@ -29,7 +29,7 @@ struct ArrayList<T> {
     inner: Array<T>,
 }
 
-impl <T: Debug + Default + Clone> ArrayList<T> {
+impl<T: Debug + Default + Clone + PartialEq> ArrayList<T> {
     pub fn new() -> ArrayList<T> {
         ArrayList {
             length: 0,
@@ -40,29 +40,50 @@ impl <T: Debug + Default + Clone> ArrayList<T> {
     fn grow_inner(&mut self) {
         let prev = &self.inner;
         let mut new = vec![T::default(); prev.len() * 2];
-        for i in 0..(prev.len()-1) {
+        for i in 0..prev.len() {
             new[i] = prev[i].clone();
         }
         self.inner = new;
     }
 
     pub fn append(&mut self, item: T) {
-        println!("adding {item:?} to {self:?}");
+        println!("appending {item:?} to {self:?}");
         if self.inner.len() == self.length {
             self.grow_inner();
         }
         self.inner[self.length] = item;
         self.length = self.length + 1;
-        dbg!(&self);
     }
 
-    pub fn remove(&mut self) -> T {
+    pub fn pop(&mut self) -> T {
         let tail = self.length - 1;
         let item = self.inner[tail].clone();
         println!("removing {item:?} from {self:?}");
         self.inner[tail] = T::default();
         self.length = tail;
         item
+    }
+
+    pub fn remove_at(&mut self, index: usize) -> Option<T> {
+        if index < self.length {
+            let item = self.inner[index].clone();
+            for i in index..self.length {
+                self.inner[i] = self.inner[i+1].clone();
+            }
+            self.length = self.length - 1;
+            Some(item)
+        } else {
+            None
+        }
+    }
+
+    pub fn remove(&mut self, item: &T) -> Option<T> {
+        for i in 0..self.length {
+            if &self.inner[i] == item {
+                return self.remove_at(i);
+            }
+        }
+        None
     }
 
     pub fn get(&self, i: usize) -> Option<&T> {
@@ -72,8 +93,24 @@ impl <T: Debug + Default + Clone> ArrayList<T> {
             None
         }
     }
-}
 
+    pub fn insert_at(&mut self, index: usize, item: T) {
+        println!("inserting {item:?} at {index:?} into {self:?}");
+        if self.inner.len() == self.length {
+            self.grow_inner();
+        }
+        for i in ((index+1)..=self.length).rev() {
+            self.inner[i] = self.inner[i-1].clone();
+        }
+        self.inner[index] = item;
+        self.length = self.length + 1;
+    }
+
+    pub fn prepend(&mut self, item: T) {
+        println!("prepending {item:?} to {self:?}");
+        self.insert_at(0, item);
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -88,10 +125,25 @@ mod tests {
         l.append(4);
         l.append(5);
         l.append(-1);
-        let item = l.remove();
+        assert_eq!(Some(&5), l.get(4));
+        let item = l.pop();
         assert_eq!(-1, item);
         assert_eq!(5, l.length);
         assert_eq!(Some(&3), l.get(2));
         assert_eq!(None, l.get(l.length));
+        l.prepend(1000);
+        assert_eq!(Some(&1000), l.get(0));
+        assert_eq!(Some(&1), l.get(1));
+        assert_eq!(Some(&2), l.get(2));
+        assert_eq!(Some(&5), l.get(l.length - 1));
+        assert_eq!(6, l.length);
+        l.insert_at(1, 2000);
+        assert_eq!(Some(&2000), l.get(1));
+        assert_eq!(Some(2000), l.remove_at(1));
+        assert_eq!(6, l.length);
+        assert_eq!(Some(&5), l.get(l.length - 1));
+        assert_eq!(Some(3), l.remove(&3));
+        assert_eq!(None, l.remove(&500));
+        assert_eq!(5, l.length);
     }
 }
