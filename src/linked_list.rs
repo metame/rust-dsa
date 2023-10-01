8881932c1772da334208791423def9e622a1663b
Michael J Erwin <michaeljameserwin@gmail.com>
2023-10-01 15:52:51 -0400

    LinkedList done

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -4,34 +4,30 @@
  * Insert and remove from head/edges O(1)/constant time
  * get, Traversal is O(N)
  * new
- * add
+ * push_front
  * get
- * remove
+ * pop_front
  */
-use std::cell::{Ref, RefCell};
-use std::ops::Deref;
-use std::rc::Rc;
-
-type Cell<T> = Rc<RefCell<Node<T>>>;
+use std::boxed::Box;
 
 pub struct LinkedList<T> {
-    head: Option<Cell<T>>,
+    head: Option<Box<Node<T>>>,
     len: usize,
 }
 
 struct Node<T> {
     val: T,
-    next: Option<Cell<T>>,
+    next: Option<Box<Node<T>>>,
 }
 
-impl<T: Clone> LinkedList<T> {
+impl<T> LinkedList<T> {
     pub fn new() -> LinkedList<T> {
         LinkedList { head: None, len: 0 }
     }
 
-    pub fn add(&mut self, val: T) {
-        let prev_head = if let Some(cell) = &self.head {
-            Some(Rc::clone(&cell))
+    pub fn push_front(&mut self, val: T) {
+        let prev_head = if let Some(node) = self.head.take() {
+            Some(node)
         } else {
             None
         };
@@ -40,21 +36,35 @@ impl<T: Clone> LinkedList<T> {
             next: prev_head,
         };
         self.len += 1;
-        self.head = Some(Rc::new(RefCell::new(n)));
+        self.head = Some(Box::new(n));
     }
 
-    pub fn get(&self, index: usize) -> Option<T> {
+    pub fn pop_front(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None;
+        }
+        let mut head = self.head.take().unwrap();
+        let new_head = if let Some(node) = head.next.take() {
+            Some(node)
+        } else {
+            None
+        };
+
+        self.len -= 1;
+        self.head = new_head;
+        Some(head.val)
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
         if index >= self.len {
             return None;
         }
-        let mut cell = self.head.clone().unwrap();
+        let mut cell = self.head.as_ref().unwrap();
         for _ in 1..=index {
-            let next_cell = cell.borrow().deref().next.clone().unwrap();
+            let next_cell = cell.next.as_ref().unwrap();
             cell = next_cell;
         }
-        cell.borrow();
-        let val = Ref::clone(&cell.borrow()).deref().val.clone();
-        Some(val)
+        Some(&cell.val)
     }
 }
 
@@ -66,12 +76,17 @@ mod tests {
     fn linked_list_works() {
         let mut l = LinkedList::<isize>::new();
         assert_eq!(0, l.len);
-        l.add(200);
-        l.add(-2);
-        l.add(4123);
+        assert_eq!(None, l.pop_front());
+        l.push_front(200);
+        l.push_front(-2);
+        l.push_front(4123);
         assert_eq!(3, l.len);
-        assert_eq!(Some(200), l.get(2));
-        assert_eq!(Some(-2), l.get(1));
-        assert_eq!(Some(4123), l.get(0));
+        assert_eq!(Some(&200), l.get(2));
+        assert_eq!(Some(&-2), l.get(1));
+        assert_eq!(Some(&4123), l.get(0));
+        assert_eq!(Some(4123), l.pop_front());
+        assert_eq!(Some(-2), l.pop_front());
+        assert_eq!(Some(200), l.pop_front());
+        assert_eq!(None, l.pop_front());
     }
 }
